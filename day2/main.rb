c9412aa46e5e24408f4c4209a9091a805f818c76
Edward Tam <edward.tam@flipp.com>
2023-12-02 01:23:06 -0500

    touch up

--- day2/main.rb
+++ day2/main.rb
@@ -11,23 +11,26 @@ CUBES = {
 def part_1
   ids = []
 
-
   File.readlines('data.txt').each do |line|
     valid = true
 
     id = line.chomp.split(':').first.split(' ').last.to_i
     
-    shows = line.chomp.split(':').last.split(';').map(&:strip)
-
-    shows.each do |show|
-      show.split(',').each do |cubes|
-        val, color = cubes.split(' ')
-        if CUBES[color] < val.to_i
-          valid = false
-          break
+    line
+      .chomp
+      .split(':')
+      .last
+      .split(';')
+      .map(&:strip)
+      .each do |show|
+        show.split(',').each do |cubes|
+          val, color = cubes.split(' ')
+          if CUBES[color] < val.to_i
+            valid = false
+            break
+          end
         end
       end
-    end
 
     ids << id if valid
   end
@@ -39,21 +42,24 @@ def part_2
   powers = []
 
   File.readlines('data.txt').each do |line|
-    shows = line.chomp.split(':').last.split(';').map(&:strip)
 
     cubes = {}
 
-    shows.each do |show|
-
-
-      show.split(',').each do |shown|
-        val, color = shown.split(' ')
-        
-        if cubes[color].nil? || cubes[color] < val.to_i
-          cubes[color] = val.to_i
+    line
+      .chomp
+      .split(':')
+      .last
+      .split(';')
+      .map(&:strip)
+      .each do |show|
+        show.split(',').each do |shown|
+          val, color = shown.split(' ')
+          
+          if cubes[color].nil? || cubes[color] < val.to_i
+            cubes[color] = val.to_i
+          end
         end
       end
-    end
 
     powers << cubes.values.reduce(:*)
   end
