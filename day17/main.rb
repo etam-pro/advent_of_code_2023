324e9db9e85143228c6f040999faac2aca02dccb
Edward Tam <edward.tam@flipp.com>
2023-12-18 00:03:39 -0500

    Day 17 Part 1

---
+++ day17/main.rb
@@ -0,0 +1,184 @@
+# frozen_string_literal: true
+
+require 'debug'
+
+require 'active_support/core_ext/object/deep_dup'
+
+class Node
+  attr_accessor :value, :x, :y, :visited, :dist, :prevs
+
+  def initialize(value, x, y)
+    @value = value
+    @x = x
+    @y = y
+    @dist = Float::INFINITY
+    @prevs = {}
+  end
+
+  def xy
+    [x, y]
+  end
+end
+
+class Graph
+  attr_accessor :nodes, :min
+
+  DIRS = {
+    up: [0, -1],
+    down: [0, 1],
+    left: [-1, 0],
+    right: [1, 0]
+  }
+
+  def initialize(nodes)
+    @nodes = nodes
+    @min = Float::INFINITY
+
+    @dist = {}
+    @prev = {}
+    @search = 0
+  end
+
+  def start
+    @nodes[0][0]
+  end
+
+  def finish
+    @nodes.last.last
+  end
+
+  def max_x
+    @nodes.first.size - 1
+  end
+
+  def max_y
+    @nodes.size - 1
+  end
+
+  def out_of_bounds?(cur, dir)
+    x_dir, y_dir = dir
+
+    return true if cur.x + x_dir < 0 || cur.y + y_dir < 0
+    return true if cur.x + x_dir > max_x || cur.y + y_dir > max_y
+
+    false
+  end
+
+  def bfs
+    start.prevs[[]] = 0
+    queue = [[start, []]]
+
+    until queue.empty?
+
+      cur, prevs = queue.shift
+
+      puts "#{cur.xy}"
+
+      if cur == finish
+        if cur.prevs[prevs] < @min
+          @min = cur.prevs[prevs]
+          print  "#{@min} \r"
+        end
+
+        next
+      end
+
+      next if cur.prevs[prevs] && cur.prevs[prevs] >= @min
+
+      dirs = if cur == start
+               [DIRS[:down], DIRS[:right]]
+             else
+               prev_x, prev_y = prevs.last
+               dir = [cur.x - prev_x, cur.y - prev_y]
+               possible_dirs(dir)
+             end
+
+      dirs
+        .filter { |new_dir| !out_of_bounds?(cur, new_dir) }
+        .filter { |new_dir| can_move?(cur, prevs, new_dir) }
+        .each do |new_dir|
+          x_dir, y_dir = new_dir
+
+          next_node = @nodes[cur.y + y_dir][cur.x + x_dir]
+
+          alt = next_node.value + cur.prevs[prevs]
+          next_prevs = prevs.last(2) + [cur.xy]
+
+          next if next_node.prevs[next_prevs] && next_node.prevs[next_prevs] <= alt
+
+          next_node.prevs[next_prevs] = alt
+          queue << [next_node, next_prevs]
+        end
+    end
+
+    finish.prevs.values.min
+  end
+
+  def possible_dirs(cur_dir)
+    dirs = []
+
+    case cur_dir
+    when DIRS[:up]
+      dirs << DIRS[:left]
+      dirs << DIRS[:right]
+      dirs << DIRS[:up]
+    when DIRS[:down]
+      dirs << DIRS[:left]
+      dirs << DIRS[:right]
+      dirs << DIRS[:down]
+    when DIRS[:left]
+      dirs << DIRS[:up]
+      dirs << DIRS[:down]
+      dirs << DIRS[:left]
+    when DIRS[:right]
+      dirs << DIRS[:up]
+      dirs << DIRS[:down]
+      dirs << DIRS[:right]
+    end
+
+    dirs
+  end
+
+  def same_dirs_in_a_row(cur, prevs, dir)
+    count = 0
+    ns = (prevs + [cur.xy]).reverse
+    ns.each_with_index do |n, i|
+      break if n.nil?
+      break if ns[i + 1].nil?
+
+      cur_x, cur_y = n
+      prev_x, prev_y = ns[i + 1]
+
+      cur_dir = [cur_x <=> prev_x, cur_y <=> prev_y]
+      break if cur_dir != dir
+
+      count += 1
+    end
+    count
+  end
+
+  def can_move?(cur, prevs, dir, _ultra = false)
+    count = same_dirs_in_a_row(cur, prevs, dir)
+
+    count < 3
+  end
+end
+
+def parse(filename)
+  nodes = []
+
+  File.readlines(filename).map(&:chomp).each_with_index do |line, y|
+    nodes << line.split('').map.with_index { |c, x| Node.new(c.to_i, x, y) }
+  end
+
+  Graph.new(nodes)
+end
+
+def part1
+  graph = parse('day17/sample.txt')
+  min = graph.bfs
+
+  puts min
+end
+
+part1
